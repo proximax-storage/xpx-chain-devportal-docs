605491ad5f7cb3516defd0a5316dcdbdb234dbf5
Chun Lam <chunlam.sees@gmail.com>
2025-04-17 19:24:51 +0800

    update tag line

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,7 +22,7 @@ const users = [
 
 const siteConfig = {
   title: 'Developer Center', // Title for your website.
-  tagline: 'A website for blockchain developers',
+  tagline: "Sirius blockchain platform's official documentation hub",
   url: 'https://bcdocs.xpxsirius.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
